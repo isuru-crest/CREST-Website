6107c6ce9e8d7835243d76efdca3cea81375e813
Tran Nguyen <trannguyen@192-168-1-113.tpgi.com.au>
2021-02-14 20:51:33 +1030

    Removed research areas, added link to CREST DLT page

--- src/pages/area4.js
+++ src/pages/area4.js
@@ -9,6 +9,7 @@ import Section, { Row, Col } from '../components/ui/wrapper'
 import Heading from '../components/ui/heading'
 import Sidebar from '../containers/blog/sidebar'
 import AreaArea from '../containers/blog/area-area/area4'
+import Button from '../components/ui/button'
 import CTA from '../containers/global/cta-area/section-one'
 
 const AreaListLargeImage = ({ pageContext, location, ...restProps }) => {
@@ -30,20 +31,12 @@ const AreaListLargeImage = ({ pageContext, location, ...restProps }) => {
                             <p>The last decade has witnessed the proliferation of data collection and artificial intelligence in our daily lives. Technological advances under the Internet of Things moniker have extended the scope of data collection beyond our interactions with the digital world to cover real-world phenomena. The next evolution would emerge when intelligent machines can coordinate to carry out complex processes with minimal human involvement. Correctly done, such a machine economy can bring about tremendous productivity and profound impacts on our lives. The machine economy hinges on integration and security. Traditionally, the integration happens via intermediaries such as If This Then That (IFTTT), which resolve discrepancies between services' interfaces and coordinate them according to predefined scenarios. This approach is unsustainable in the machine economy. A better approach would be to let intelligent machines coordinate themselves and make the process transparent, traceable, and verifiable.</p>
                             <p>In recent years, the Distributed Ledger technology (DLT), also known as Blockchain, has emerged as a blueprint for a new way of orchestrating intelligent machines and services in a decentralized, transparent, and verifiable way. Whether DL can make an effective solution depends a great deal on how it is integrated with other systems. Moreover, as DL is a software system itself, the successful utilization of DL also depends on how it is designed and engineered. As any emerging technologies, DLT has its terminologies, characteristics, and constraints that software engineers must carefully consider in order to utilize it effectively.</p>
                             <p>The goal of this research area is to support industries, governments, and societies in leveraging DLT to address current challenges and prepare for the emerging machine economy. </p>
+                            
                         </Col>
                     </Row>
                     <Row style={{paddingTop: "50px"}}>
                         <Col>
-                            <h2>Topic Areas</h2>
-                        </Col>
-                    </Row>
-                    <Row>
-                        {/* <Col lg={{ span: 4, order: 1 }} xs={{ span: 12, order: 2 }}>
-                            <Sidebar />
-                        </Col> */}
-                        <Col >
-                            {/* <Heading {...headingStyle}>Interesting articles <span>updated daily</span></Heading> */}
-                            <AreaArea />
+                            <Button to={`http://dlt.crest-centre.net`} hover="2">Find Out More</Button>
                         </Col>
                     </Row>
                 </Section>
