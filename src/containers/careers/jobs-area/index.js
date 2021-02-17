24a2bab82e909103b2ce3f1cf8b20a9ab707463a
william <williamjiangsa@gmail.com>
2021-02-17 18:36:21 +1030

    Closed Position

--- src/containers/careers/jobs-area/index.js
+++ src/containers/careers/jobs-area/index.js
@@ -20,22 +20,68 @@ const JobsArea = () => {
                     link
                 }
             }
+            closedjobs: careersJson(id: {eq: "our-closed-jobs"}) {
+                closedjobs {
+                    id
+                    title
+                    type
+                    description
+                    link
+                }
+            }
         }      
     `);
-    const { intro: { title }, jobs: { jobs } } = getJobsData
+    const { intro: { title }, jobs: { jobs }, closedjobs: { closedjobs } } = getJobsData
 
     return (
         <SectionWrap>
             <Container>
-                <Row justify="center">
-                    <Col lg={7}>
-                        <SectionTitle title={title} />
-                    </Col>
+                {jobs[0].id != "null" && <div>
+                    <Row justify="center">
+                        <Col lg={7}>
+                            <SectionTitle title={title} />
+                        </Col>
+                    </Row>
+                    {/* <Row justify="left">
+                        <SectionTitle title={"Current Positions"} />
+                    </Row> */}
+                    <Row>
+                        <Col lg={12}>
+                            <JobList>
+                                {jobs.map((job, i) => {
+                                    const isEven = i % 2 === 0;
+                                    return (
+                                        <JobItem key={job.id} isEven={isEven}>
+                                            <Row>
+                                                <Col lg={3}>
+                                                    <JobTitle>{job.title}</JobTitle>
+                                                    <JobType>{job.type}</JobType>
+                                                </Col>
+                                                <Col lg={6}>
+                                                    <JobDesc>{job.description}</JobDesc>
+                                                </Col>
+                                                <Col lg={3}>
+                                                    <JobButton>
+                                                        <Button to={job.link} varient="outlined" bordercolor="#ddd">Detail</Button>
+                                                    </JobButton>
+                                                </Col>
+                                            </Row>
+                                        </JobItem>
+                                    )
+                                })}
+                            </JobList>
+                        </Col>
+                    </Row>
+                </div>}
+
+
+                <Row justify="left">
+                    <SectionTitle title={"Closed Positions"} />
                 </Row>
                 <Row>
                     <Col lg={12}>
                         <JobList>
-                            {jobs.map((job, i) => {
+                            {closedjobs.map((job, i) => {
                                 const isEven = i % 2 === 0;
                                 return (
                                     <JobItem key={job.id} isEven={isEven}>
@@ -49,7 +95,7 @@ const JobsArea = () => {
                                             </Col>
                                             <Col lg={3}>
                                                 <JobButton>
-                                                    <Button to={job.link} varient="outlined" bordercolor="#ddd">Get Started</Button>
+                                                    <Button to={job.link} varient="outlined" bordercolor="#ddd">Detail</Button>
                                                 </JobButton>
                                             </Col>
                                         </Row>
